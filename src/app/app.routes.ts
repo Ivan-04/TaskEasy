a97b336bb1a16382a022cd4c9d1425948900ff7c
Asus <i.t.nachkov@abv.bg>
2026-01-28 19:30:38 +0200

    Creating model for user, register logic and login logic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './login/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: '', pathMatch: 'full', redirectTo: 'register' },
+    { path: '**', redirectTo: 'home' }
+];
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { RouterModule, Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss',
+    standalone: true,
+    imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSnackBarModule
+  ],
+})
+export class LoginComponent {
+    hidePassword = true;
+    form;
+    
+    constructor(
+        private fb: FormBuilder,
+        private auth: AuthService,
+        private router: Router,
+        private snack: MatSnackBar
+    ) {
+        this.form = this.fb.group({
+        username: ['', [Validators.required, Validators.minLength(4)]],
+        password: ['', [Validators.required, Validators.minLength(6)]]
+        });
+    }
+
+    submit() {
+        if(this.form.invalid) return;
+
+        const username = this.form.value.username!;
+        const password = this.form.value.password!;
+
+        this.auth.login(username, password).subscribe(res => {
+            this.snack.open(res.message, 'OK', { duration: 2500 });
+            if (res.ok) this.router.navigate(['/home']);
+        });
+    }
+}
